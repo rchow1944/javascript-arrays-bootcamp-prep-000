8f18ff7764a8d2d666f6c54f6d8414bb885b674e
Raymond Chow <raychow1944@yahoo.com>
2018-11-16 02:18:18 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -7,3 +7,14 @@ function destructivelyAddElementToBeginningOfArray(arr, el){
   arr.unshift(el);
   return arr;
 }
+
+function addElementToEndOfArray(arr, el){
+  arr = [el, ...arr];
+  return arr;
+}
+
+function destructivelyAddElementToBeginningOfArray(arr, el){
+  arr.unshift(el);
+  return arr;
+}
+
